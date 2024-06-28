daf164b348d49050a20f42ca03f4eb367c8a0498
Charan Mandapati <mandapaticharan@gmail.com>
2024-06-28 16:11:53 +0200

    descriptions and keyword scripts

--- descriptions.py
+++ descriptions.py
@@ -1,7 +1,6 @@
 from openai import OpenAI
+from dotenv import find_dotenv, load_dotenv
 import pandas as pd
-import numpy as np
-import requests
 import os
 
 dotenvpath = find_dotenv()
@@ -9,27 +8,73 @@ load_dotenv(dotenvpath)
 
 OPENAI_API_KEY = os.getenv('OpenAI_KEY')
 
-response = requests.get("http://127.0.0.1:5000/keyword-ideas?keyword=mens socks")
-print(response.text)
-
 client = OpenAI(api_key = OPENAI_API_KEY)
 
-mTitle = "Mens Socks"
-mDescription = "Men"
+df = pd.read_csv("product_keywords.csv")
 
-completion = client.chat.completions.create(
-    model="gtp-40",
-    messages=[
-        {
-            "role": "system",
-            "content": "Extract SEO keywords from the provided product title and description. These keywords should be ones that people might search online to find this product. Provide the keywords separated by comma but no space after comma"
-        },
-        {
-            "role": "user"
-            "content": "Product Title:" "Product description:" 
-        }
-    ]
-)
-print(completion.choices[0].message)
+new_df = pd.DataFrame(columns=['SKU', 'ProductDescription', 'ProductMetaTitle', 'ProductMetaDescription', 'SEOKeywords', 'OptimisedMetaTitle', 'OptimisedMetaDescription', 'OptimisedDescription'])
+print(type(new_df))
 
+# For each row in our CSV file, we'll find relevant SEO keywords and optimise our meta data and product description.
+for index, row in df.iterrows():
+    description = row["ProductDescription_en"]
+    mTitle = row["ProductMetaTitle_en"]
+    mDescription = row["ProductMetaDescription_en"]
+    sku = row["ProductNumber"]
+    seoKeywords = row["SEOKeywords"]
+
+    # Optimise content using the SEO keywords
+    print("Starting optimisation of meta title, meta description and product description")
+    generateMTitle = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "system",
+                "content": "Generate a new meta title based on the provided product title and SEO keywords. The meta title should be strictly less than 60 characters long."
+            },
+            {
+                "role": "user",
+                "content": f"Product Title: {mTitle}\nSEO Keywords: {seoKeywords}"
+            }
+        ]
+    )
+    optimisedMetaTitle = generateMTitle.choices[0].message.content
+
+    generateMDescription = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "system",
+                "content": "Generate a new meta description based on the provided product title and SEO keywords. The description should be strictly less than 155 characters long."
+            },
+            {
+                "role": "user",
+                "content": f"Product Title: {mDescription}\nSEO Keywords: {seoKeywords}"
+            }
+        ]
+    )
+    optimisedMetaDescription = generateMDescription.choices[0].message.content
+
+    generateDescription = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "system",
+                "content": "Generate a new product description based on the provided product title and SEO keywords. Keep the description to 300 characters long. Avoid mentioning any other brands except for happysocks"
+            },
+            {
+                "role": "user",
+                "content": f"Product Title: {mDescription}\nSEO Keywords: {seoKeywords}"
+            }
+        ]
+    )
+    optimisedDescription = generateDescription.choices[0].message.content
+
+    # Store the information in the new data frame
+    new_row = {'SKU': sku, 'ProductDescription': description, 'ProductMetaTitle': mTitle, 'ProductMetaDescription': mDescription, 'SEOKeywords': seoKeywords, 'OptimisedMetaTitle': optimisedMetaTitle, 'OptimisedMetaDescription': optimisedMetaDescription, 'OptimisedDescription': optimisedDescription}
+    new_df = pd.concat([new_df, pd.DataFrame([new_row])], ignore_index=True)
+    
+
+print("Optimisation complete! File saved as optimised_products.csv")
+new_df.to_csv("optimised_products.csv", index=False)
 
---
+++ keywords.py
@@ -0,0 +1,59 @@
+from openai import OpenAI
+import json
+from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import requests
+import os
+
+dotenvpath = find_dotenv()
+load_dotenv(dotenvpath)
+
+OPENAI_API_KEY = os.getenv('OpenAI_KEY')
+client = OpenAI(api_key = OPENAI_API_KEY)
+
+df = pd.read_csv("outlets_products.csv")
+
+new_df = pd.DataFrame(columns=['SKU', 'ProductDescription', 'ProductMetaTitle', 'ProductMetaDescription', 'SEOKeywords'])
+print(type(new_df))
+
+# For each row in our CSV file, we'll find relevant SEO keywords and optimise our meta data and product description.
+for index, row in df.iterrows():
+    description = row["ProductDescription_en"]
+    mTitle = row["ProductMetaTitle_en"]
+    mDescription = row["ProductMetaDescription_en"]
+    sku = row["ProductNumber"]
+
+    # Extract initial set of keywords from the product title and description
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "system",
+                "content": "Extract SEO keywords from the provided product title and description. These keywords should be ones that people might search online to find this product. Provide the keywords separated by comma but no space after comma"
+            },
+            {
+                "role": "user",
+                "content": f"Product Title: {mTitle}\nProduct description: {mDescription}"
+            }
+        ]
+    )
+    keywords = completion.choices[0].message.content
+    print("Extracted keywrods from the product title and description")
+
+    # Get SEO keywrods from the API
+    try:
+        response = requests.get(f"http://127.0.0.1:5000/keyword-ideas?keyword={keywords}")
+    except:
+        print("Error in getting SEO keywords from the API")
+    
+    print("Got SEO keywords from the API")
+    output = json.loads(response.text)
+    seoKeywords = ', '.join(output)
+
+    new_row = {'SKU': sku, 'ProductDescription': description, 'ProductMetaTitle': mTitle, 'ProductMetaDescription': mDescription, 'SEOKeywords': seoKeywords }
+    new_df = pd.concat([new_df, pd.DataFrame([new_row])], ignore_index=True)
+
+    
+# Save the keyword and product date to new csv file
+new_df.to_csv("product_keywords.csv", index=False)
+print("Saved the product data to product_keywords.csv file")
